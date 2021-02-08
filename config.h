d967b4297970a54a5953a487304f74a611d6b19a
donotturnoff <admin@donotturnoff.net>
2021-02-08 17:30:59 +0000

    Custom configuration

---
+++ config.h
@@ -0,0 +1,19 @@
+static const char *background_color = "#222222";
+static const char *border_color = "#005577";
+static const char *font_color = "#eeeeee";
+static const char *font_pattern = "Terminus:size=10:antialias=false:autohint=true";
+static const unsigned line_spacing = 5;
+static const unsigned int padding = 15;
+
+static const unsigned int width = 190;
+static const unsigned int border_size = 1;
+static const unsigned int pos_x = 20;
+static const unsigned int pos_y = 38;
+
+enum corners { TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT };
+enum corners corner = TOP_RIGHT;
+
+static const unsigned int duration = 0; /* in seconds */
+
+#define DISMISS_BUTTON Button1
+#define ACTION_BUTTON Button3
